d6d661ae14b8620914b77f1709d16a281d8830f7
thentrsfs <filipstojkov54@gmail.com>
2026-03-26 20:09:03 +0100

    Change hero text position on mobile.

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-     <div className="relative w-full h-[75vh]">
-       <div className="absolute z-10 top-1/2 left-8 md:left-36 -translate-y-1/2 text-white max-w-xl flex flex-col gap-5">
+     <div className="relative w-full lg:h-[75vh] h-[70vh]">
+       <div className="absolute z-10 lg:top-1/2 bottom-  left-8 md:left-36 lg:-translate-y-1/2 text-white lg:max-w-xl flex flex-col gap-5">
 
   <h3 className="text-sm md:text-base font-medium text-background">
     Kadeřnictví v Pardubicích
@@ -14,7 +14,7 @@ const Hero = () => {
     Studio Aura
   </h1>
 
-  <p className="text-white/70 text-base md:text-lg max-w-md">
+  <p className="text-white/70 text-base md:text-lg lg:max-w-md">
     Moderní střihy, barvení a individuální péče
   </p>
 
@@ -31,7 +31,7 @@ const Hero = () => {
 </div>
      <Image src={'/images/barber-hero.jpg'} alt="Man haircut" fill className="object-cover object-[80%]" loading="eager" />
      <div className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-[#000000]/90 to-[#000000]/10"/>
-     <div className="absolute top-0 left-0 w-full h-1/2 bg-linear-to-b from-[#000000]/30 to-transparent"/>
+     <div className="absolute top-0 left-0 w-full h-1/2 bg-linear-to-b from-[#000000]/50 to-transparent"/>
      <div className="absolute bottom-0 left-0 w-full h-20 bg-linear-to-b from-transparent to-background/30"/>
      <div className="absolute bottom-0 left-0 w-full h-full bg-linear-to-b from-transparent to-background/15"/>
      </div>
