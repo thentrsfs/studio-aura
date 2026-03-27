0f6eb6dad9c3eb0a6486f3d0f8d7a404071137c1
thentrsfs <filipstojkov54@gmail.com>
2026-03-27 16:02:43 +0100

    Polish services section and changing arrows style to match the style of the website.

--- components/sections/Služby.tsx
+++ components/sections/Služby.tsx
@@ -10,23 +10,26 @@ import {
 
 const Služby = () => {
   return (
-    <div id="sluzby" className="py-16 px-6 md:px-36" >
-        <h2 className="lg:text-4xl text-3xl text-black font-sora font-semibold uppercase mb-6">Naše služby</h2>
-        <p className="lg:text-lg text-black/70">Vytváříme styl, který vám sedí – od střihu až po kompletní péči o vlasy.</p>
+    <div id="sluzby" className="px-6 md:px-36 min-h-dvh flex flex-col justify-center relative" >
+      <div className="absolute top-16 left-0 w-full px-6 md:px-36">
+        <h2 className="lg:text-4xl text-3xl text-black font-sora font-bold uppercase mb-4 relative">Naše <span className="text-primary">služby</span>
+          <span className="block w-12 h-1 bg-primary mt-0.5"></span>
+        </h2>
+        <p className="lg:text-lg text-black/60 max-w-md">Vytváříme styl, který vám sedí – od střihu až po kompletní péči o vlasy.</p>
+        </div>
      <Carousel className="mt-10 relative">
   <CarouselContent className="cursor-grab active:cursor-grabbing">
     {services.map((service) => (
-      <CarouselItem key={service.id} className=" lg:basis-1/3">
-        <div className="relative h-100 md:h-125 overflow-hidden rounded-lg group">
+      <CarouselItem key={service.id} className=" lg:basis-1/3 ">
+        <div className="relative h-100 md:h-125 overflow-hidden rounded-xl group ">
           <Image
             src={service.image}
             alt={service.name}
             fill
             sizes="100%"
-            className="object-cover rounded-lg group-hover:scale-105 transition-transform duration-300 "
+            className="object-cover rounded-xl group-hover:scale-105 transition-all duration-300 hover:brightness-85"
           />
-
-          <div className="absolute bottom-0 left-0 w-full h-35 bg-black/40 group-hover:bg-black/60 backdrop-blur-xs p-6 flex flex-col rounded-b-lg justify-center gap-2 transition-all duration-300 select-none">
+          <div className="absolute bottom-0 left-0 w-full h-35 bg-black/40 group-hover:bg-black/60 backdrop-blur-xs p-6 flex flex-col rounded-b-lg justify-center gap-2 transition-all duration-300 select-none ">
             <h3 className="lg:text-2xl text-xl text-white font-sora font-semibold">
               {service.name}
             </h3>
@@ -37,8 +40,8 @@ const Služby = () => {
     ))}
   </CarouselContent>
 
-  <CarouselPrevious className="p-5 lg:-left-20 left-2 cursor-pointer hover:bg-black/5 bg-background transition-all duration-300 disabled:opacity-0 opacity-100 " />
-  <CarouselNext className="p-5 lg:-right-20 right-2 cursor-pointer hover:bg-black/5 bg-background transition-all duration-300 disabled:opacity-0 opacity-100 " />
+  <CarouselPrevious className="p-5 cursor-pointer hover:scale-110 border-0 text-white left-4 top-1/2 -translate-y-1/2 bg-primary hover:bg-primary-hover shadow-md transition-all duration-300 disabled:opacity-0 opacity-100 " />
+  <CarouselNext className="p-5 cursor-pointer hover:scale-110 border-0 text-white right-4 top-1/2 -translate-y-1/2 bg-primary hover:bg-primary-hover shadow-md transition-all duration-300 disabled:opacity-0 opacity-100 " />
 </Carousel>
     </div>
   )
