1b65559be541a42a124bbf0dfa25f920e86ed91b
thentrsfs <filipstojkov54@gmail.com>
2026-04-02 10:00:08 +0200

    Update NavDots to dynamically generate navigation dots based on the number of sections in the page.

--- components/ui/NavDots.tsx
+++ components/ui/NavDots.tsx
@@ -1,9 +1,12 @@
 
 
 const NavDots = ({active, theme} : {active: number, theme: string}) => {
+
+  const sections = ["#hero", "#sluzby", "#cenik", "#galerie"];
+
   return (
         <div className="fixed right-6 top-1/2 -translate-y-1/2 hidden lg:flex flex-col gap-3 z-50">
-  {[0,1,2,3].map((i) => (
+  {sections.map((_,i) => (
     <button
       key={i}
       onClick={() => {
