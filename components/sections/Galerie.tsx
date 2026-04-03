eed8dab733b58d702d1bb421be5c17308c741e92
thentrsfs <filipstojkov54@gmail.com>
2026-04-03 13:46:30 +0200

    Add lightbox from 'Yet Another React Lightbox' to galerie section.

--- components/sections/Galerie.tsx
+++ components/sections/Galerie.tsx
@@ -1,6 +1,10 @@
+'use client'
 import Image from "next/image"
+import { useState } from "react"
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
-const images = [
+export const images = [
     {src: '/images/galerie/galerie1.jpg', alt: 'Image 1'},
     {src: '/images/galerie/galerie2.jpg', alt: 'Image 2'},
     {src: '/images/galerie/galerie3.jpg', alt: 'Image 3'},
@@ -10,6 +14,14 @@ const images = [
 ]
 
 const Galerie = () => {
+    const [lightboxOpen, setLightboxOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState<number | undefined>(undefined);
+
+    const openLightbox = (index: number) => {
+        setSelectedImage(index);
+        setLightboxOpen(true);
+    }
+
   return (
     <section data-theme="light" id="galerie" className="min-h-dvh bg-background px-6 md:px-36 flex flex-col gap-10 relative py-10">
         <div>
@@ -17,16 +29,20 @@ const Galerie = () => {
             <p className="lg:text-lg text-black/60 max-w-md">Ukázky naší práce.</p>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             {images.map((image, index) => (
-                <div key={index} className="relative group overflow-hidden rounded-lg cursor-pointer w-full lg:h-80">
-                    <Image src={image.src} alt={image.alt} fill sizes="100%" className="object-cover"  />
-                    <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 transition flex items-center justify-center">
-    <span className="text-white text-sm">Zobrazit</span>
+                <div onClick={() => openLightbox(index)} key={index} className="relative group overflow-hidden rounded-lg cursor-pointer w-full h-60 lg:h-80">
+                    <Image src={image.src} alt={image.alt} fill sizes="100%" className="object-cover group-hover:scale-105 transition-all duration-300"  />
+                    <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition flex items-center justify-center">
+    <span className="text-white text-sm border border-white/70 px-4 py-2 rounded-full bg-black/50">Zobrazit</span>
   </div>
                 </div>
             ))}
         </div>
+            <Lightbox slides={images} open={lightboxOpen} close={() => setLightboxOpen(false)} index={selectedImage} styles={{ container: { backgroundColor: "rgba(0, 0, 0, .8)" }, slide: {
+      borderRadius: "16px",
+      overflow: "hidden",
+    },}} />
     </section>
   )
 }
--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -37,7 +37,7 @@ const Nav = ({active, theme, isMenuOpen, setIsMenuOpen} : {active: number, theme
 <span className="group-hover:w-full w-0 h-px bg-white transition-all duration-300 "></span>
 </div>
 </div>
-<CustomButton/>
+<CustomButton className="max-lg:hidden"/>
 <button onClick={() => setIsMenuOpen(!isMenuOpen)} className={`text-lg ${isMenuOpen ? 'text-black pr-3' : 'text-white'} font-medium lg:hidden font-sora z-26`}>{isMenuOpen ? <IoClose className="text-3xl" /> : 'Menu'}</button>
 
 <button onClick={() => setIsMenuOpen(!isMenuOpen)} className={`text-lg ${isMenuOpen && 'text-black'} font-medium ${active === 0 ? 'opacity-0' :'opacity-100'} ${theme === 'dark' ? 'text-white' : 'text-black'} fixed right-6 top-10 lg:hidden font-sora z-26 transition-all duration-300`}>Menu</button>
